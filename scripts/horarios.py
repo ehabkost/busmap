792f8baa5e6972f97dc0c33209b39bb2f040937f
Eduardo Habkost <ehabkost@raisama.net>
2007-10-21 09:44:11 -0200

    Fetch all bus schedules from the page
    Signed-off-by: Eduardo Habkost <ehabkost@raisama.net>

--- scripts/horarios.py
+++ scripts/horarios.py
@@ -3,16 +3,37 @@ import env
 import re
 
 MAIN_PAGE = 'http://www.curitiba.pr.gov.br/Servicos/Transporte/HorarioOnibus/ctba.asp'
+def url_horario(cod):
+	return 'http://www.curitiba.pr.gov.br/Servicos/Transporte/HorarioOnibus/HorariosOnibusUrbano.asp?LINHA='+cod
+
 re_item_lista = re.compile(u"""<OPTION VALUE='(.*?)'>.*?<FONT .*?>(.*?)</FONT>""",
 	re.MULTILINE|re.DOTALL)
 
 def lista_linhas():
+	print 'Fetching main page...'
 	f = env.urlopener.open(MAIN_PAGE)
 	s = f.read()
 	for cod,nome in re_item_lista.findall(s):
 		nome = nome.rstrip()
+		print 'Found list item: %s:%s' % (cod, nome)
 		yield cod,nome
 
 
-for cod,nome in lista_linhas():
-	print '%s\t%s' % (cod, nome)
+def get_horarios(cod):
+	print 'Getting schedules for %s' % (cod)
+	f = env.urlopener.open(url_horario(cod))
+	return f.read()
+
+fl = open('linhas.txt', 'w')
+linhas = list(lista_linhas())
+for cod,nome in linhas:
+	fl.write('%s\t%s\n' % (cod, nome))
+	fl.flush()
+fl.close()
+
+for cod,nome in linhas:
+	hor = get_horarios(cod)
+
+	fh = open('hor/%s.html' % (cod), 'w')
+	fh.write(hor)
+	fh.close()
